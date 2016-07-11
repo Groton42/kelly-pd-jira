cbc486af922f705da483d32495a12da18ecfbea0
Luke Epp <lucasfepp@gmail.com>
2016-07-11 09:52:05 -0700

    Better debugging

--- web/pd2jira.php
+++ web/pd2jira.php
@@ -118,8 +118,8 @@ if ($messages) {
       $url = "https://$pd_subdomain.pagerduty.com/api/v1/incidents/$incident_id";
       $return = http_request($url, "", "GET", "token", "", $pd_api_token);
       if ($return['status_code'] == '200') {
-        $response = json_decode($return['response'], true);
-        error_log(json_encode($response->incident->assignments));
+        $response = json_decode($return['response']);
+        error_log(json_encode($response->incident));
         $pd_requester_id = $response->incident->assignments[0]->assignee->id;
         error_log($pd_requester_id);
       }
